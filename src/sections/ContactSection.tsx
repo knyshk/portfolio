7a33929f5b719fb0d6ceaae61246c74d9c25428e
Kanishk_Jain_0510 <kanishk.jain0510@gmail.com>
2026-06-16 17:04:09 +0530

    security

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Mail, MapPin, Phone, Newspaper } from 'lucide-react';
 import { BentoItem, Magnet, Reveal } from '../components/Animations';
 
